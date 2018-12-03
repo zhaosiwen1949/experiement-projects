2b4f34134e70e626063e086118679aba92123b3d
caoys <caoys@xiaoshouyi.com>
2018-12-03 16:21:14 +0800

    将build目录纳入到版本控制系统中

---
+++ test-yarn-push/build/precache-manifest.76a08e9e36098a322fb750bd8c3de48f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6aaf3a9d57231fd68aa6",
+    "url": "/static/js/main.6aaf3a9d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "6aaf3a9d57231fd68aa6",
+    "url": "/static/css/main.8d117e84.chunk.css"
+  },
+  {
+    "revision": "c84d594477c1141a4a45ee53cc16204b",
+    "url": "/index.html"
+  }
+];
