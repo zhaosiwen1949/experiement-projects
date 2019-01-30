5098383e5cb8f3f2f991925e52425f3e1e4d2ae5
caoys <caoys@xiaoshouyi.com>
2019-01-30 18:30:54 +0800

    验证循环引用问题

---
+++ test-loop-import/src/a.js
@@ -0,0 +1,30 @@
+import b from './b';
+// export function foo() {
+//   console.log('foo');
+// };
+// export function bar() {
+//   console.log('bar');
+// };
+
+// export const foo = 'foo';
+// export const bar = 'bar';
+
+b();
+// console.log(b)
+
+export const foo = () => {
+  console.log('foo');
+};
+export const bar = () => {
+  console.log('bar');
+};
+
+// export function foo() {
+//   console.log('foo');
+// };
+// export function bar() {
+//   console.log('bar');
+// };
+
+// export const foo = 'foo';
+// export const bar = 'bar';
---
+++ test-loop-import/src/b.js
@@ -0,0 +1,14 @@
+import { foo, bar } from './a';
+const b = () => {
+  console.log('I am BBBBB~~~~');
+  foo();
+  bar();
+  // console.log(foo);
+  // console.log(bar);
+}
+
+// const b = 'I am BBBBB~~~~';
+// console.log(foo);
+// console.log(bar);
+
+export default b;
